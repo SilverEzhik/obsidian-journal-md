276849d9914e8a14c9c398083b499c11dfa61a2c
Hedgehog <i@ezhik.me>
2024-12-07 07:55:59 +0000

    Make a new plugin

--- main.ts
+++ main.ts
@@ -1,81 +1,134 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, SuggestModal } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface JournalMdSettings {
+	automaticDateHeadings: boolean;
+	headingDateFormat: string;
+	locale: string | null;
+	journalNotePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: JournalMdSettings = {
+	automaticDateHeadings: true,
+	headingDateFormat: "YYYY-MM-DD (ddd)",
+	locale: null,
+	journalNotePath: "Journal"
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class JournalMd extends Plugin {
+	settings: JournalMdSettings;
+
+	get locale() {
+		return this.settings.locale ?? window.moment.locale();
+	}
+
+	get noteName() {
+		return this.settings.journalNotePath;
+	}
+	get notePath() {
+		return this.settings.journalNotePath + ".md";
+	}
+
+	async getJournalNote() {
+		const path = this.notePath;
+		let note = this.app.vault.getFileByPath(path);
+		if (!note) {
+			new Notice(`Journal note at ${path} not found. Creating it now.`);
+			await this.app.vault.createFolder(path.split("/").slice(0, -1).join("/"));
+			note = await this.app.vault.create(path, "");
+		}
+		return note;
+	}
+
+	async openJournalNote() {
+		const note = await this.getJournalNote();
+		this.app.workspace.getLeaf(false).openFile(note);
+	}
+
+	async insertDateHeading() {
+		const note = await this.getJournalNote();
+		const date = window.moment().locale(this.locale).format(this.settings.headingDateFormat);
+
+		const headings = this.app.metadataCache.getFileCache(note)?.headings ?? [];
+		const firstHeading = headings[0];
+
+		if (firstHeading?.heading === date && firstHeading?.level === 1) {
+			return;
+		}
+
+		const heading = `# ${date}\n`;
+		const content = await this.app.vault.read(note);
+
+		// for all we know the file was changed outside of Obsidian so let's play it safe
+		if (content.startsWith(heading)) {
+			return;
+		}
+
+		await this.app.vault.modify(note, `${heading}${content}`);
+	}
+
+	async appendToLastEntry(text: string) {
+		const note = await this.getJournalNote();
+		const cache = this.app.metadataCache.getFileCache(note);
+		const headings = cache?.headings;
+		let offset = headings?.[1]?.position?.start?.offset;
+
+		const content = await this.app.vault.read(note);
+		offset ??= content.length;
+
+		let before = content.slice(0, offset);
+		const after = content.slice(offset);
+
+		const firstHeadingEnd = headings?.[0]?.position?.end?.offset;
+		if (firstHeadingEnd && before.slice(firstHeadingEnd).trim() === "") {
+			before = before.slice(0, firstHeadingEnd);
+		}
+
+		await this.app.vault.modify(note, `${before}\n${text}\n${after}`);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.addRibbonIcon("notebook", "Journal", () => this.openJournalNote());
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "open-journal-note",
+			name: "Open journal",
+			callback: () => this.openJournalNote()
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.addCommand({
+			id: "quick-append-to-journal",
+			name: "Quick append to journal",
+			callback: async () => {
+				try {
+					const text = await getTextToAppend(this.app);
+					await this.appendToLastEntry(text);
+				} catch (e) {
+					// user cancelled
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerObsidianProtocolHandler("journal/open", async ({ text }) => {
+			await this.openJournalNote();
+		});
+		this.registerObsidianProtocolHandler("journal/append", async ({ text }) => {
+			await this.appendToLastEntry(text);
+			await this.openJournalNote();
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new JournalMdSettingTab(this.app, this));
+
+		// automatically insert a date heading
+		this.app.workspace.on("file-open", async (file) => {
+			if (this.settings.automaticDateHeadings && file?.path === this.notePath) {
+				this.insertDateHeading();
+			}
+		});
 	}
 
 	onunload() {
@@ -91,44 +144,130 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class JournalMdSettingTab extends PluginSettingTab {
+	plugin: JournalMd;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: JournalMd) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName("Automatically create date headings")
+			.setDesc("Will automatically create date headings in the journal file.")
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.automaticDateHeadings)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.automaticDateHeadings = value;
 					await this.plugin.saveSettings();
-				}));
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Heading date format")
+			.setDesc((() => {
+				const i = document.createDocumentFragment();
+				i.appendText("For more syntax, refer to ");
+				i.createEl(
+					"a", {
+					text: "format reference",
+					attr: {
+						href: "https://momentjs.com/docs/#/displaying/format/",
+						target: "_blank"
+					}
+				});
+				return i;
+			})())
+			.addMomentFormat(format => format
+				.setDefaultFormat(DEFAULT_SETTINGS.headingDateFormat)
+				.setValue(this.plugin.settings.headingDateFormat)
+				.onChange(async (value) => {
+					this.plugin.settings.headingDateFormat = value;
+					await this.plugin.saveSettings();
+				})
+			);
+
+
+
+		new Setting(containerEl)
+			.setName("Heading date locale")
+			.addDropdown(dropdown => dropdown
+				.addOption("", "Use Obsidian language")
+				.addOptions(
+					Object.fromEntries(window.moment.locales().sort().map(locale => [locale, locale]))
+				)
+				.setValue(this.plugin.settings.locale ?? "")
+				.onChange(async (value) => {
+					if (value === "") {
+						this.plugin.settings.locale = null;
+					} else {
+						this.plugin.settings.locale = value;
+					}
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Journal note")
+			.setDesc("This note will be used as the journal.")
+			.addText(text => text
+				.setPlaceholder("Journal")
+				.setValue(this.plugin.settings.journalNotePath)
+				.onChange(async (value) => {
+					this.plugin.settings.journalNotePath = value;
+					await this.plugin.saveSettings();
+				})
+			);
 	}
+}
+
+class AppendToJournalModal extends SuggestModal<string> {
+	getSuggestions(query: string): string[] {
+		if (query.length == 0) {
+			return [];
+		} else {
+			return [query];
+		}
+	}
+
+	renderSuggestion(value: string, el: HTMLElement) {
+		el.appendText(value);
+	}
+
+	onChooseSuggestion(item: string, evt: MouseEvent | KeyboardEvent) {
+		this.resolve(item);
+		this.resolved = true;
+	}
+
+	onClose() {
+		setTimeout(() => {
+			if (!this.resolved) {
+				this.reject();
+			}
+		}, 1000);
+	}
+
+	resolved = false;
+
+	resolve: (value?: string | PromiseLike<string>) => void;
+	reject: (reason?: string) => void;
+
+	constructor(app: App, resolve: (value?: string | PromiseLike<string>) => void, reject: (reason?: string) => void) {
+		super(app);
+		this.resolve = resolve;
+		this.reject = reject;
+
+		this.setPlaceholder("Append to journal...");
+		this.emptyStateText = "...";
+	}
+}
+
+function getTextToAppend(app: App): Promise<string> {
+	return new Promise((resolve, reject) => {
+		new AppendToJournalModal(app, resolve, reject).open();
+	});
 }
